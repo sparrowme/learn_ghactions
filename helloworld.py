a3d6964c28c604deba60372ba363c78440d7a735
Todd Sparrow <todd@sparrowmail.net>
2021-10-27 16:47:04 -0400

    test fail for 3.6

--- helloworld.py
+++ helloworld.py
@@ -6,6 +6,8 @@ def main():
     if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
         # let's make this script fail for python 3.6
         x = 1/0
+    if sys.platform == 'darwin':
+        x = 1/0
 
 
 if __name__ == "__main__":
