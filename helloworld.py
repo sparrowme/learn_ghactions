5f4abb48b27affaabf13b2e4bbf22288def1a888
Todd Sparrow <todd@sparrowmail.net>
2021-10-27 18:17:52 -0400

    fix print

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import sys
 
 
 def main():
-    print(f"Hello World!" + str(sys.version_info) + str(sys.platform))
+    print("Hello World!" + str(sys.version_info) + str(sys.platform))
     if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
         # let's make this script fail for python 3.6
         x = 1/0
