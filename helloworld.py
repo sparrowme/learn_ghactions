30767929cf75df75031b07f76e38a0cc41e851c8
Todd Sparrow <todd@sparrowmail.net>
2021-10-27 15:58:40 -0400

    test fail for 3.6

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
+import sys
+
+
 def main():
     print("Hello World!")
+    if sys.version_info == (3.6):
+        # let's make this script fail for python 3.6
+        x = 1/0
 
 
 if __name__ == "__main__":
