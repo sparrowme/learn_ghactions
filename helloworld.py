adeabc4fd855fba37e2270ef60d10006f825ebd1
Todd Sparrow <todd@sparrowmail.net>
2021-10-27 19:48:10 -0400

    remove os matrix; add exption fail for py 3.6

--- helloworld.py
+++ helloworld.py
@@ -5,9 +5,7 @@ def main():
     print("Hello World!" + str(sys.version_info) + str(sys.platform))
     if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
         # let's make this script fail for python 3.6
-        x = 1/0
-    if sys.platform == 'darwin':
-        x = 1/0
+        raise Exception('Python version 3.6.x is unsupported!')
 
 
 if __name__ == "__main__":
